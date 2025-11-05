5add9020f061de3ac4e97a4d383077ebb67f461a
acinox-it <sahraoui.abdeldjalil@outlook.com>
2025-11-05 17:06:25 +0100

    Ajout de basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/todolist'>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
